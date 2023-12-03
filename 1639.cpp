c7edea50af72093ffb8a9bda228c7112327ab7e2
aziz <mdtareqa90@gmail.com>
2023-12-03 20:08:01 +0600

    Chocolate 2

---
+++ 1639.cpp
@@ -0,0 +1,30 @@
+// Author : Mars_Coder
+
+
+#define _USE_MATH_DEFINES
+#include<bits/stdc++.h>
+using namespace std;
+#define PI    3.141592653589793238462643383279502884L
+#define ln "\n" // no flush, oppos of endl
+#define _flush endl
+#define stop_sync ios::sync_with_stdio(false) // not to use c-stye io
+#define untie_ios cin.tie(nullptr) // no flush
+#define sz(x) (x).size()
+#define lli long long int
+#define FIXED(x) cout << fixed << setprecision(x)
+
+int main(void){
+	
+	#ifndef ONLINE_JUDGE
+		freopen("input.txt", "r", stdin);
+		freopen("output.txt", "w", stdout);
+		freopen("error.txt", "w", stderr);
+	#endif
+	
+	int n, m;
+	cin >> n >> m;
+	if((n * m) % 2){
+		cout << "[second]=:]" << ln;
+	}else cout << "[:=[first]" << ln;
+	return 0;
+}
