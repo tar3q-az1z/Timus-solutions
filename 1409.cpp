7a746e8b95bccdc79cdebac46e552c87f45affbd
aziz <mdtareqa90@gmail.com>
2023-12-03 20:16:59 +0600

    Two Gangsters

---
+++ 1409.cpp
@@ -0,0 +1,27 @@
+// Author : Mars_Coder
+
+
+#define _USE_MATH_DEFINES
+#include<bits/stdc++.h>
+using namespace std;
+#define PI    3.141592653589793238462643383279502884L
+#define ln "\n" // no flush, oppos of endl
+#define _flush endl
+#define stop_sync ios::sync_with_stdio(false) // not to use c-stye io
+#define untie_ios cin.tie(nullptr) // no flush
+#define lli long long int
+#define FIXED(x) cout << fixed << setprecision(x)
+
+int main(void){
+	
+	#ifndef ONLINE_JUDGE
+		freopen("input.txt", "r", stdin);
+		freopen("output.txt", "w", stdout);
+		freopen("error.txt", "w", stderr);
+	#endif
+		
+	int h, l;
+	cin >> h >> l;
+	cout << (h + l - 1) - h << ' ' << (h + l - 1) - l << ln;
+	return 0;
+}
