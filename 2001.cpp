27610fc25f471bf32c9b73734a1e1eab966e5743
aziz <mdtareqa90@gmail.com>
2023-12-03 20:13:10 +0600

    Mathematicians and Berries

---
+++ 2001.cpp
@@ -0,0 +1,27 @@
+// Author : Mars_Coder
+
+
+#define _USE_MATH_DEFINES
+#include<bits/stdc++.h>
+using namespace std;
+#define PI    3.141592653589793238462643383279502884L
+#define ln "\n" // no flush, oppos of endl
+#define _flush endl
+#define stop_sync ios::sync_with_stdio(false) // not to use c-stye io
+#define untie_ios cin.tie(nullptr) // no flush
+#define lli long long int
+#define FIXED(x) cout << fixed << setprecision(x)
+
+int main(void){
+	
+	#ifndef ONLINE_JUDGE
+		freopen("input.txt", "r", stdin);
+		freopen("output.txt", "w", stdout);
+		freopen("error.txt", "w", stderr);
+	#endif
+	
+	int a1, b1, a2, b2, a3, b3;
+	cin >> a1 >> b1 >> a2 >> b2 >> a3 >> b3;
+	cout << (b3 - b1) << ' ' << (a2 - a1) << ln;	
+	return 0;
+}
