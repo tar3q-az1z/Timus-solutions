21651380c37a8cc9a07752fc24cdca1deaab40ee
aziz <mdtareqa90@gmail.com>
2023-12-03 20:13:33 +0600

    Bicycle Codes

---
+++ 1877.cpp
@@ -0,0 +1,28 @@
+// Author : Mars_Coder
+
+
+#define _USE_MATH_DEFINES
+#include<bits/stdc++.h>
+using namespace std;
+#define PI    3.141592653589793238462643383279502884L
+#define ln "\n" // no flush, oppos of endl
+#define _flush endl
+#define stop_sync ios::sync_with_stdio(false) // not to use c-stye io
+#define untie_ios cin.tie(nullptr) // no flush
+#define lli long long int
+#define FIXED(x) cout << fixed << setprecision(x)
+
+int main(void){
+	
+	#ifndef ONLINE_JUDGE
+		freopen("input.txt", "r", stdin);
+		freopen("output.txt", "w", stdout);
+		freopen("error.txt", "w", stderr);
+	#endif
+	
+	int f, s;
+	cin >> f >> s;
+	if((f % 2 == 0) || (s % 2)) cout << "yes\n";
+	else cout << "no\n";
+	return 0; 
+}
